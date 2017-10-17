c908572aa8cac5e397f1171a153f0c41ae38badf
Holly Wilkalis and Tyler Bussanich <holly+tyler@>
2017-10-17 10:20:03 -0700

    Add buttons to change background color of page

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,23 @@
 $(document).ready(function() {
   $(".clickable").click(function() {
-    $("#walrus-showing").fadeIn();
-    $("#walrus-hidden").fadeOut();
+    $("#walrus-showing").fadeToggle();
+    $("#walrus-hidden").fadeToggle();
+  });
+});
+
+$(document).ready(function() {
+  $("button#green").click(function() {
+    $("body").removeClass();
+    $("body").addClass("green-background");
+  });
+
+  $("button#yellow").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
+  });
+
+  $("button#red").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
   });
 });
